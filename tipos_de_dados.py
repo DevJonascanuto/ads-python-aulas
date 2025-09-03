79d7d906e977bde37ad9ae9959c82e84ff3fbf83
unknown <jonascanuto1@gmail.com>
2025-09-02 23:38:42 -0300

    tipos de dados

---
+++ tipos_de_dados.py
@@ -0,0 +1,22 @@
+# tipos de dados em python
+
+# inteiros sao numeros inteiro
+
+idade = 25
+print (type(idade))
+
+# float numero com ponto
+peso = 85.5
+print (type(peso))
+
+
+
+# strings texto dentro de aspas
+nome = 'joao'
+print (type(nome))
+
+#boleano = verdadeiro ou falso
+
+estudando = True
+print (type(estudando))
+print (estudando)
